737d66e84b9d2fea2ffe493cce367800f5d95994
DESKTOP-1N5612G\it <gusgh1324@naver.com>
2024-02-26 10:07:09 +0900

    test1

---
+++ src/main/java/com/mechu/MechuApplication.java
@@ -0,0 +1,13 @@
+package com.mechu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mechu/MechuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mechu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MechuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
