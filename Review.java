71ac12264a9b6152166ed8808f5c4f2d14645785
JJoak <150228730+JJoak@users.noreply.github.com>
2024-02-26 13:08:33 +0900

    Add files via upload
    Signed-off-by: JJoak <150228730+JJoak@users.noreply.github.com>

---
+++ Review.java
@@ -0,0 +1,44 @@
+package src.main.java.com.mechu.dto;
+
+public class Review {
+
+//	상호 
+	private String storeName;
+//	이름 
+	private String userName;
+//	리뷰개수
+	private int reviewCount;
+//	등록일 
+	private int date;
+//	댓글수 
+	private int cmtcount;
+//	추천수 
+	private int cmdcount;
+//	이미지
+	private String reviewimg;
+//	내용
+	private String review;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+// ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
+//	별점 
+//	star 1~5
+//	별에 마우스 올리면 색 채워지도록
+
+//	평균 
+//	점수 총 합 % 리뷰 수
+//
+//	리뷰 남기기
+//	버튼 / href
+
+}
